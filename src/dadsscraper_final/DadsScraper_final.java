e7bd6a5ccd6d47f952df6a9958e51b7b2f3b4f35
tjbuckner <teejay.buckner@gmail.com>
2014-11-04 03:26:07 -0500

    initial commit

---
+++ src/dadsscraper_final/DadsScraper_final.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dadsscraper_final;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.*;
+
+/**
+ *
+ * @author TJ
+ */
+public class DadsScraper_final {
+
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        jFrameMain jfMain = new jFrameMain();
+        jfMain.setVisible(true);
+        
+//        JFrame jFrameMain = new JFrame();
+//        jFrameMain.setLayout(new GridLayout(3,1));
+//        jFrameMain.setSize(400, 400);
+//        
+//        JPanel jPanelBottom = new JPanel();
+//
+//        JTable jTableData = new JTable();
+//        JLabel jLabelProgress = new JLabel("PROGRESS");
+//
+//        JButton jButtonRun = new JButton("RUN");
+//        JButton jButtonExport = new JButton("EXPORT");
+//
+//        jPanelBottom.add(jButtonRun);
+//        jPanelBottom.add(jButtonExport);
+//        
+//        jFrameMain.add(jTableData);
+//        jFrameMain.add(jLabelProgress);
+//        jFrameMain.add(jPanelBottom);
+//        
+//        jFrameMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        jFrameMain.setVisible(true);
+    
+    }
+}
